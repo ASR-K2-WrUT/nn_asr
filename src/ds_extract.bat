2ac7ce80f65d852ca98f56c8edfd0f05d46f4e40
Jerzy.Sas <jerzy.sas@pwr.edu.pl>
2018-04-16 17:57:25 +0200

    reinitializing

--- src/ds_extract.bat
+++
@@ -1,4 +0,0 @@
-python %CLARIN_ROOT%\src\ds_extract.py train\train.nn.bin train.set.%2.bin %1 %2
-python %CLARIN_ROOT%\src\ds_extract.py test\valid.nn.bin valid.set.%2.bin %1 %3
-python %CLARIN_ROOT%\src\ds_estract.py core\core.nn.bin core.set.%2.bin %1 %3
-
